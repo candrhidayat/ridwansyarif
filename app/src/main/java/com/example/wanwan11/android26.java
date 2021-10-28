34d9b7306e4c79b9c18ca2a936b14bbafd233d46
candra <hidayatcandra110398@gmail.com>
2021-10-29 00:42:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wanwan11/android26.java
@@ -0,0 +1,14 @@
+package com.example.wanwan11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class android26 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android26);
+    }
+}
